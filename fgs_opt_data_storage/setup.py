a4dfa4572b429389fb9fc84849561e549e6ab37b
fugashy <fugashy@icloud.com>
2018-12-16 16:49:36 +0900

    Rename opt data package

--- fgs_opt_data_storage/setup.py
+++ fgs_opt_data_storage/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['fgs_opt_data_generator'],
+    packages=['fgs_opt_data_storage'],
     package_dir={'': 'src'},
 )
 
