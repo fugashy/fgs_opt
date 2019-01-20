1eb44fd43dae89614a0f697733df3bb670902115
fugashy <fugashy@icloud.com>
2019-01-20 14:55:11 +0900

    Modified context of optimization

--- fgs_g2o_playground/include/fgs_g2o_playground/optimization_context.hpp
+++ fgs_g2o_playground/include/fgs_g2o_playground/optimization_context.hpp
@@ -1,13 +1,11 @@
 #ifndef FGS_G2O_PLAYGROUND_OPTIMIZATION_CONTEXT_HPP_
 #define FGS_G2O_PLAYGROUND_OPTIMIZATION_CONTEXT_HPP_
 
-#include "g2o/core/block_solver.h"
-#include "g2o/solvers/dense/linear_solver_dense.h"
-#include "g2o/core/optimization_algorithm_levenberg.h"
-#include "g2o/core/sparse_optimizer.h"
+
 #include <opencv2/core.hpp>
 
 #include "fgs_g2o_playground/curve.hpp"
+#include "fgs_g2o_playground/optimizer.hpp"
 
 namespace fgs {
 namespace g2o_playground {
@@ -26,27 +24,19 @@ void CurveFitting(const cv::Mat& cv_data_array) {
   param->Init(100.0);
   param->ShowParam();
 
-  // Cereate optimizer
-  typedef g2o::BlockSolver<g2o::BlockSolverTraits<Eigen::Dynamic, Eigen::Dynamic>>
-      MyBlockSolver;
-  typedef g2o::LinearSolverDense<MyBlockSolver::PoseMatrixType>
-      MyLinearSolver;
-  g2o::SparseOptimizer optimizer;
-  g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(
-    g2o::make_unique<MyBlockSolver>(g2o::make_unique<MyLinearSolver>()));
-  optimizer.setAlgorithm(solver);
+  auto optimizer = Optimizer::Create("levenberg");
 
   param->setId(0);
-  optimizer.addVertex(param);
+  optimizer->addVertex(param);
 
   for (int i = 0; i < cv_data_array.rows; ++i) {
     data_array[i]->setVertex(0, param);
-    optimizer.addEdge(data_array[i]);
+    optimizer->addEdge(data_array[i]);
   }
 
-  optimizer.initializeOptimization();
-  optimizer.setVerbose(true);
-  optimizer.optimize(1000);
+  optimizer->initializeOptimization();
+  optimizer->setVerbose(true);
+  optimizer->optimize(1000);
 
   std::cout << "After" << std::endl;
   param->ShowParam();
