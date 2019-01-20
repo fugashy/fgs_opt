39a183e05c212b4bb3a177974d98bb865bad1f24
fugashy <fugashy@icloud.com>
2019-01-20 15:06:16 +0900

    Add line optimization option

--- fgs_g2o_playground/src/fgs_g2o_playground/fitting.cpp
+++ fgs_g2o_playground/src/fgs_g2o_playground/fitting.cpp
@@ -1,15 +1,17 @@
-#include "fgs_g2o_playground/optimization_context.hpp"
-#include "fgs_g2o_playground/curve.hpp"
-
 #include <fgs_opt_data_storage/loader.hpp>
 
+#include "fgs_g2o_playground/curve.hpp"
+#include "fgs_g2o_playground/line.hpp"
+#include "fgs_g2o_playground/optimization_context.hpp"
+
 using fgs::g2o_playground::CurveFitting;
+using fgs::g2o_playground::LineFitting;
 using fgs::opt_data_storage::LoadCVYaml;
 
 int main(int argc, char** argv) {
   if (argc != 3) {
-    std::cout << "usage: rosrun fgs_ceres_playground "
-                 "fgs_ceres_playground_fitting data_path line2d" << std::endl;
+    std::cout << "usage: rosrun fgs_g2o_playground "
+                 "fgs_g2o_playground_fitting data_path curve" << std::endl;
     return -1;
   }
   cv::Mat data = LoadCVYaml(argv[1]);
@@ -17,6 +19,8 @@ int main(int argc, char** argv) {
 
   if (type == "curve") {
     CurveFitting(data);
+  } else if (type == "line") {
+    LineFitting(data);
   } else {
     std::cerr << type << " is not implemented" << std::endl;
     return -1;
