7bcc53022f4733effc54244beab3e7b75b62157a
fugashy <fugashy@icloud.com>
2019-02-28 20:52:48 +0900

    Modify to load complex data

--- fgs_opt_data_storage/src/fgs_opt_data_storage/loader.cpp
+++ fgs_opt_data_storage/src/fgs_opt_data_storage/loader.cpp
@@ -1,15 +1,34 @@
+#include <iostream>
 #include "fgs_opt_data_storage/loader.hpp"
 
 namespace fgs {
 namespace opt_data_storage {
 
-cv::Mat LoadCVYaml(const std::string& file_path) {
+std::vector<cv::Mat> LoadCVYaml(const std::string& file_path) {
   cv::FileStorage fs(file_path, cv::FileStorage::READ);
 
-  cv::Mat out;
-  fs["data"] >> out;
+  std::vector<cv::Mat> return_data;
 
-  return out;
+  // read data
+  cv::Mat data;
+  fs["data"] >> data;
+  return_data.push_back(data);
+
+  // read extended data
+  // this is for problems such as bundle-adjustment
+  cv::FileNode ed_fn = fs["extended_data"];
+  if (ed_fn.empty()) {
+    return return_data;
+  }
+  if (ed_fn.type() != cv::FileNode::SEQ) {
+    throw std::runtime_error("extended_data should be sequential type");
+  }
+  for (cv::FileNodeIterator it = ed_fn.begin(); it != ed_fn.end(); ++it) {
+    cv::Mat data;
+    (*it)["data"] >> data;
+    return_data.push_back(data);
+  }
+  return return_data;
 }
 
 }
