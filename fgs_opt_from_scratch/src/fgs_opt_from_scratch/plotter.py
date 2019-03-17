1cfb5f433a6c69d4d27bb3f0a2f557b6678f7d33
fugashy <fugashy@icloud.com>
2019-03-17 20:07:01 +0900

    Remove unused file

--- fgs_opt_from_scratch/src/fgs_opt_from_scratch/plotter.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-def scatter2d(data):
-    transpose = np.array(data).T
-    plt.scatter(list(transpose[0]), list(transpose[1]))
-    plt.show()
