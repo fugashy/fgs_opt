2493dc630c0851d12242f3d99e39184a3bbc3d69
fugashy <fugashy@icloud.com>
2019-03-17 15:21:56 +0900

    Add levenberg-marquardt

--- fgs_opt_from_scratch/src/fgs_opt_from_scratch/updaters.py
+++ fgs_opt_from_scratch/src/fgs_opt_from_scratch/updaters.py
@@ -6,6 +6,9 @@ import numpy.linalg as LA
 def create(config_dict):
     if config_dict['type'] == 'gauss_newton':
         return GaussNewton()
+    elif config_dict['type'] == 'levenberg_marquardt':
+        weight = config_dict['weight']
+        return LevenbergMarquardt(weight)
     else:
         raise NotImplementedError(
                 '{} is not implemented'.format(config_dict['type']))
@@ -43,5 +46,48 @@ class GaussNewton():
 
         # 更新ベクトル
         delta = np.dot(np.dot(AHInv, J.T), R).tolist()
+
+        return delta
+
+
+class LevenbergMarquardt():
+    u"""
+    レーベンバーグマーカート法を用いて
+    データからパラメータを最適に近づけるパラメータを算出するクラス
+    """
+    def __init__(self, weight):
+        u"""
+        Args:
+            weight: 更新に勾配に掛け合わせる重み(float)
+        """
+        self.__weight = weight
+
+    def update(self, model, data):
+        u"""
+        最急降下法法でパラメータを更新する
+        Args:
+            model: データが従っているとされるモデル(modelsモジュール)
+            data: 観測データ(numpy.array)
+        Returns:
+            更新されたパラメータ(numpy.array)
+        """
+        # ヤコビアン(パラメータ数 x パラメータ自由度)
+        J = np.zeros((len(data), len(model.get_param())))
+        for i in range(len(data)):
+            J[i] = model.gradient(data[i])
+
+
+        # 近似されたヘッセ行列(J.TとJの内積)
+        AH = np.dot(J.T, J)
+
+        # 近似されたヘッセ行列の対角成分のみを残した行列
+        DAH = np.diag(AH.diagonal())
+
+        # 残差ベクトル
+        R = np.array([model.residual(
+            data[i]) for i in range(len(data))]).T
+
+        # 更新パラメータ
+        delta = np.dot(np.dot(LA.pinv(AH + self.__weight * DAH), J.T), R)
 
         return delta
