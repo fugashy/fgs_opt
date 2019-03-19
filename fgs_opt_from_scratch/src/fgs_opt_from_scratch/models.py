2798bd104e2fdc808ea31fba3134540c8c05ede6
fugashy <fugashy@icloud.com>
2019-03-19 22:31:50 +0900

    Refactoring of models

--- fgs_opt_from_scratch/src/fgs_opt_from_scratch/models.py
+++ fgs_opt_from_scratch/src/fgs_opt_from_scratch/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
 
 def create(config_dict):
     if config_dict['type'] == 'michaelis_menten':
@@ -11,119 +12,66 @@ def create(config_dict):
         raise NotImplementedError(
             'type {] is not implemented'.format(config_dict['type']))
 
+
+class Model(object):
+    def __init__(self):
+        # パラメータ
+        self.p = []
+        # モデル式
+        self.f = lambda x, p: np.inf
+        # 残差式
+        self.r = lambda x, p: np.inf
+        # 残差勾配
+        self.rg = lambda x, p: [np.inf]
+
+    def update(self, p):
+        if len(p) != len(self.p):
+            print('Invalid dof({}). We ignore this updation.'.format(len(p)))
+            return
+        self.p = p
+
+    def get_param(self):
+        return deepcopy(self.p)
+
+    def residual(self, x):
+        return self.r(x, self.p)
+
+    def gradient(self, x):
+        return self.rg(x, self.p)
+
 # https://ja.wikipedia.org/wiki/%E3%82%AC%E3%82%A6%E3%82%B9%E3%83%BB%E3%83%8B%E3%83%A5%E3%83%BC%E3%83%88%E3%83%B3%E6%B3%95#%E4%BE%8B
-class MichaelisMentenEquation:
+# x1 = p0*x0 / (p1 + x0)
+class MichaelisMentenEquation(Model):
     def __init__(self):
-        self.__b = [0., 0.]
-        # v(s) = b0s/(b2 + s)
-        self.__v = lambda s, b: b[0]*s / (b[1] + s)
-        # 残差
-        self.__r = lambda x, b: x[1] - self.__v(x[0], b)
-        # 残差の勾配
-        self.__dfdb0 = lambda s, b: -s / (b[1] + s)
-        self.__dfdb1 = lambda s, b: b[0]*s / (b[1] + s)**2
-
-    def update(self, b):
-        u"""
-        パラメータを更新する
-
-        Args:
-            b: ミカエリス・メンテン式のパラメータ(list)
-
-        Returns:
-            なし
-        """
-        if len(b) != 2:
-            print('Invalid dof. We ignore this updation.')
-            return
-
-        self.__b = b
-
-    def get_param(self):
-        u"""
-        パラメータを得る
-
-        Args:
-            なし
-
-        Returns:
-            パラメータ(list)
-        """
-        return self.__b
-
-    def residual(self, x):
-        u"""
-        入力データに対する残差を計算する
-
-        Args:
-            データ(list)
-
-        Returns:
-            残差(float)
-        """
-        return self.__r(x, self.__b)
-
-    def gradient(self, x):
-        u"""
-        入力データに対する勾配ベクトルを返す
-
-        Args:
-            データ(list)
-
-        Returns:
-            勾配ベクトル(list)
-        """
-        return [self.__dfdb0(x[0], self.__b), self.__dfdb1(x[0], self.__b)]
+        super(MichaelisMentenEquation, self).__init__()
+        self.p = [0., 0.]
+        self.f = lambda x, p: p[0] * x[0] / (p[1] + x[0])
+        self.r = lambda x, p: x[1] - self.f(x, p)
+        drdx0 = lambda x, p: -x[0] / (p[1] + x[0])
+        drdx1 = lambda x, p: p[0] * x[0] / (p[1] + x[0])**2
+        self.rg = lambda x, p: [drdx0(x, p), drdx1(x, p)]
 
 
-class Line2d:
+class Line2d(Model):
     def __init__(self):
-        self.__p = [0., 0.]
-        self.__f = lambda x, p: p[0]*x + p[1]
-        self.__r = lambda xy, p: xy[1] - self.__f(xy[0], p)
-        self.__dfda = lambda x, p: -x[0]
-        self.__dfdb = lambda x, p: -1.0
-
-    def update(self, p):
-        if len(p) != 2:
-            print('Invalid dof. We ignore this updation.')
-            return
-
-        self.__p = p
-
-    def get_param(self):
-        return self.__p
-
-    def residual(self, x):
-        return self.__r(x, self.__p)
-
-    def gradient(self, x):
-        return [self.__dfda(x, self.__p), self.__dfdb(x, self.__p)]
+        super(Line2d, self).__init__()
+        self.p = [0., 0.]
+        self.f = lambda x, p: p[0]*x[0] + p[1]
+        self.r = lambda x, p: x[1] - self.f(x, p)
+        drda = lambda x, p: -x[0]
+        drdb = lambda x, p: -1.0
+        self.rg = lambda x, p: [drda(x, p), drdb(x, p)]
 
 
-class Circle2d:
+class Circle2d(Model):
     def __init__(self):
+        super(Circle2d, self).__init__()
         # x, y, r
-        self.__p = [0., 0., 0.]
-        self.__f = lambda x, p: (x[0] - p[0])**2 + (x[1] - p[1])**2
-        self.__r = lambda x, p: p[2]**2 - self.__f(x, p)
+        self.p = [0., 0., 0.]
+        self.f = lambda x, p: (x[0] - p[0])**2 + (x[1] - p[1])**2
+        self.r = lambda x, p: p[2]**2 - self.f(x, p)
 
-        self.__drdx = lambda x, p: 2. * (x[0] - p[0])
-        self.__drdy = lambda x, p: 2. * (x[1] - p[1])
-        self.__drdr = lambda x, p: 2. * p[2]
-
-    def update(self, p):
-        if len(p) != 3:
-            print('Invalid dof. We ignore this updation.')
-            return
-
-        self.__p = p
-
-    def get_param(self):
-        return self.__p
-
-    def residual(self, x):
-        return self.__r(x, self.__p)
-
-    def gradient(self, x):
-        return [self.__drdx(x, self.__p), self.__drdy(x, self.__p), self.__drdr(x, self.__p)]
+        drdx = lambda x, p: 2. * (x[0] - p[0])
+        drdy = lambda x, p: 2. * (x[1] - p[1])
+        drdr = lambda x, p: 2. * p[2]
+        self.rg = lambda x, p: [drdx(x, p), drdy(x, p), drdr(x, p)]
