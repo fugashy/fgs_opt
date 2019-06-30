992a0eaefdae0727ebb580f63a602b70fe36805d
fugashy <fugashy@icloud.com>
2019-06-30 12:59:30 +0900

    Modify to use g2o(in progress)

--- fgs_g2o_playground/src/fgs_g2o_playground/optimizer.cpp
+++ fgs_g2o_playground/src/fgs_g2o_playground/optimizer.cpp
@@ -1,5 +1,7 @@
 #include "fgs_g2o_playground/optimizer.hpp"
 
+#include <memory>
+
 #include <g2o/core/block_solver.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
 #include <g2o/core/optimization_algorithm_gauss_newton.h>
@@ -11,21 +13,19 @@ namespace fgs {
 namespace g2o_playground {
 
 Optimizer::Ptr Optimizer::Create(const std::string& optimization_algo_type) {
-  typedef g2o::BlockSolver<g2o::BlockSolverTraits<Eigen::Dynamic, Eigen::Dynamic>>
-      MyBlockSolver;
-  typedef g2o::LinearSolverDense<MyBlockSolver::PoseMatrixType>
-      MyLinearSolver;
+  using MyBlockSolver = g2o::BlockSolver<g2o::BlockSolverTraits<Eigen::Dynamic, Eigen::Dynamic>>;
+  using MyLinearSolver = g2o::LinearSolverDense<MyBlockSolver::PoseMatrixType>;
 
   g2o::OptimizationAlgorithmWithHessian* opt_algo;
   if (optimization_algo_type == "levenberg") {
     opt_algo = new g2o::OptimizationAlgorithmLevenberg(
-        g2o::make_unique<MyBlockSolver>(g2o::make_unique<MyLinearSolver>()));
+        std::make_unique<MyBlockSolver>(std::make_unique<MyLinearSolver>()));
   } else if (optimization_algo_type == "gauss_newton") {
     opt_algo = new g2o::OptimizationAlgorithmGaussNewton(
-        g2o::make_unique<MyBlockSolver>(g2o::make_unique<MyLinearSolver>()));
+        std::make_unique<MyBlockSolver>(std::make_unique<MyLinearSolver>()));
   } else if (optimization_algo_type == "dogleg") {
     opt_algo = new g2o::OptimizationAlgorithmGaussNewton(
-        g2o::make_unique<MyBlockSolver>(g2o::make_unique<MyLinearSolver>()));
+        std::make_unique<MyBlockSolver>(std::make_unique<MyLinearSolver>()));
   }
 
   Ptr optimizer(new g2o::SparseOptimizer());
