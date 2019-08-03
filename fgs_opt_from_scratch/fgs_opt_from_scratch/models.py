137092c1d584ab274f875980c2779bffe0422d20
fugashy <fugashy@icloud.com>
2019-08-03 22:22:28 +0900

    Refactoring
    - Deprecate unused functions and members
    - Make members private

--- fgs_opt_from_scratch/fgs_opt_from_scratch/models.py
+++ fgs_opt_from_scratch/fgs_opt_from_scratch/models.py
@@ -47,57 +47,36 @@ class Model(object):
                 raise Exception(
                         'Order of parameter({}) is invalid'.format(len(p)))
         # パラメータ
-        self.p = p
+        self._p = p
         # モデル式
-        self.f = lambda x, p: np.inf
-        # モデル式のx0におけるテイラー展開(2次まで)
-        self.tf1 = lambda x, x0, p: np.inf
-        self.tf2 = lambda x, x0, p: np.inf
+        self._f = lambda x, p: np.inf
         # 残差式
-        self.r = lambda x, p: np.inf
-        # 残差式のx0におけるテイラー展開(2次まで)
-        self.rf1 = lambda x, x0, p: np.inf
-        self.rf2 = lambda x, x0, p: np.inf
+        self._r = lambda x, p: np.inf
         # 残差勾配
-        self.rg = lambda x, p: [np.inf]
+        self._rg = lambda x, p: [np.inf]
+        # モデル式のx0におけるテイラー展開(2次まで)
+        self._tf = [lambda x, x0, p: np.inf for i in range(2)]
 
     def fx(self, x):
-        return self.f(x, self.p)
+        return self._f(x, self._p)
 
     def update(self, p):
-        if len(p) != len(self.p):
+        if len(p) != len(self._p):
             print('Invalid dof({}). We ignore this updation.'.format(len(p)))
             return
-        self.p = p
+        self._p = p
 
     def get_param(self):
-        return deepcopy(self.p)
+        return deepcopy(self._p)
 
     def residual(self, x):
-        return self.r(x, self.p)
+        return self._r(x, self._p)
 
     def gradient(self, x):
-        return self.rg(x, self.p)
+        return self._rg(x, self._p)
 
     def taylor(self, x, x0, order):
-        if order == 1:
-            taylor_func = self.tf1
-        elif order == 2:
-            taylor_func = self.tf2
-        else:
-            raise Exception('Invalid order of taylor function({}).'
-                            'We ignore this process.'.format(order))
-        return taylor_func(x, x0, self.p)
-
-    def residual_taylor(self, x, x0, order):
-        if order == 1:
-            taylor_func = self.rf1
-        elif order == 2:
-            taylor_func = self.rf2
-        else:
-            raise Exception('Invalid order of taylor function({}).'
-                            'We ignore this process.'.format(order))
-        return taylor_func(x, x0, self.p)
+        return self._tf[order - 1](x, x0, self._p)
 
 
 # https://ja.wikipedia.org/wiki/%E3%82%AC%E3%82%A6%E3%82%B9%E3%83%BB%E3%83%8B%E3%83%A5%E3%83%BC%E3%83%88%E3%83%B3%E6%B3%95#%E4%BE%8B
@@ -105,50 +84,50 @@ class Model(object):
 class MichaelisMentenEquation(Model):
     def __init__(self, p):
         super(MichaelisMentenEquation, self).__init__(p, 2)
-        self.f = lambda x, p: p[0] * x[0] / (p[1] + x[0])
-        self.r = lambda x, p: x[1] - self.f(x, p)
+        self._f = lambda x, p: p[0] * x[0] / (p[1] + x[0])
+        self._r = lambda x, p: x[1] - self._f(x, p)
         drdx0 = lambda x, p: -x[0] / (p[1] + x[0])
         drdx1 = lambda x, p: p[0] * x[0] / (p[1] + x[0])**2
-        self.rg = lambda x, p: [drdx0(x, p), drdx1(x, p)]
+        self._rg = lambda x, p: [drdx0(x, p), drdx1(x, p)]
 
 
 class Line2d(Model):
     def __init__(self, p):
         super(Line2d, self).__init__(p, 2)
-        self.f = lambda x, p: p[0]*x[0] + p[1]
-        self.r = lambda x, p: x[1] - self.f(x, p)
+        self._f = lambda x, p: p[0]*x[0] + p[1]
+        self._r = lambda x, p: x[1] - self._f(x, p)
         drda = lambda x, p: -x[0]
         drdb = lambda x, p: -1.0
-        self.rg = lambda x, p: [drda(x, p), drdb(x, p)]
+        self._rg = lambda x, p: [drda(x, p), drdb(x, p)]
 
 
 class Circle2d(Model):
     def __init__(self, p):
         super(Circle2d, self).__init__(p, 3)
-        self.f = lambda x, p: (x[0] - p[0])**2 + (x[1] - p[1])**2
-        self.r = lambda x, p: p[2]**2 - self.f(x, p)
+        self._f = lambda x, p: (x[0] - p[0])**2 + (x[1] - p[1])**2
+        self._r = lambda x, p: p[2]**2 - self._f(x, p)
 
         drdx = lambda x, p: 2. * (x[0] - p[0])
         drdy = lambda x, p: 2. * (x[1] - p[1])
         drdr = lambda x, p: 2. * p[2]
-        self.rg = lambda x, p: [drdx(x, p), drdy(x, p), drdr(x, p)]
+        self._rg = lambda x, p: [drdx(x, p), drdy(x, p), drdr(x, p)]
 
 
 class Curve2d2Order(Model):
     def __init__(self, p):
         super(Curve2d2Order, self).__init__(p, 3)
         # y = ax**2 + bx + c
-        self.f = lambda x, p: p[0]*x[0]**2 + p[1]*x[0] + p[2]
-        self.r = lambda x, p: x[1] - self.f(x, p)
+        self._f = lambda x, p: p[0]*x[0]**2 + p[1]*x[0] + p[2]
+        self._r = lambda x, p: x[1] - self._f(x, p)
 
         drda = lambda x, p: -x[0]**2
         drdb = lambda x, p: -x[0]
         drdc = lambda x, p: -1.
-        self.rg = lambda x, p: [drda(x, p), drdb(x, p), drdc(x, p)]
+        self._rg = lambda x, p: [drda(x, p), drdb(x, p), drdc(x, p)]
 
-        self.tf1 = lambda x, x0, p: self.f(x0, p) + \
+        self._tf[0] = lambda x, x0, p: self._f(x0, p) + \
                 (2.*p[0]*x0[0] + p[1])*(x[0] - x0[0])
-        self.tf2 = lambda x, x0, p: self.tf1(x, x0, p) + \
+        self._tf[1] = lambda x, x0, p: self._tf[0](x, x0, p) + \
                 p[0]*(x[0] - x0[0])**2
 
 
@@ -156,16 +135,16 @@ class Curve2d3Order(Model):
     def __init__(self, p):
         super(Curve2d3Order, self).__init__(p, 4)
         # y = ax**3 + bx**2 + cx + d
-        self.f = lambda x, p: p[0]*x[0]**3 + p[1]*x[0]**2 + p[2]*x[0] + p[3]
-        self.r = lambda x, p: x[1] - self.f(x, p)
+        self._f = lambda x, p: p[0]*x[0]**3 + p[1]*x[0]**2 + p[2]*x[0] + p[3]
+        self._r = lambda x, p: x[1] - self._f(x, p)
 
         drda = lambda x, p: -x[0]**3
         drdb = lambda x, p: -x[0]**2
         drdc = lambda x, p: -x[0]
         drdd = lambda x, p: -1.
-        self.rg = lambda x, p: [drda(x, p), drdb(x, p), drdc(x, p), drdd(x, p)]
+        self._rg = lambda x, p: [drda(x, p), drdb(x, p), drdc(x, p), drdd(x, p)]
 
-        self.tf1 = lambda x, x0, p: self.f(x0, p) + \
+        self._tf[0] = lambda x, x0, p: self._f(x0, p) + \
                 (3.*p[0]*x0[0]**2 + 2.*p[1]*x0[0] + p[2])*(x[0] - x0[0])
-        self.tf2 = lambda x, x0, p: self.tf1(x, x0, p) + \
+        self._tf[1] = lambda x, x0, p: self._tf[0](x, x0, p) + \
                 (6.*p[0]*x0[0] + 2.*p[1])*(x[0] - x0[0])**2.
