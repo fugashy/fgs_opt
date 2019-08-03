4017945a684fd270794b3046b81211937ab79676
fugashy <fugashy@icloud.com>
2019-08-04 06:34:00 +0900

    Modify to plot parameter history

--- fgs_opt_from_scratch/fgs_opt_from_scratch/plotters.py
+++ fgs_opt_from_scratch/fgs_opt_from_scratch/plotters.py
@@ -144,23 +144,17 @@ class Param2DPlotter:
             param_range: パラメータのmin/max(list of float)
             grid_num: 分布分割数(int)
         """
-        self.__model = deepcopy(model)
-        param = self.__model.get_param()
-        if len(param) != 2:
-            raise Exception('Param2DPlotter support only 2D parameter')
+        self.__model = model
         self.__data = data
+        self._param_range = param_range
+        self._grid_num = grid_num
 
-        x = np.arange(
-                param[0] - param_range[0], param[0] + param_range[0],
-                (2. * param_range[0]) / grid_num)
-        y = np.arange(
-                param[1] - param_range[1], param[1] + param_range[1],
-                (2. * param_range[1]) / grid_num)
-        if len(x) > len(y):
-            x = np.delete(x, -1)
-        elif len(y) > len(x):
-            y = np.delete(y, -1)
-        self.__X, self.__Y = np.meshgrid(x, y)
+        # 更新毎にたどってきたパラメータの軌跡がみたい
+        self._param_history = [[], []]
+
+        self.__X = None
+        self.__Y = None
+        self._reset_range()
         self.__first = True
 
     def plot(self):
@@ -172,21 +166,56 @@ class Param2DPlotter:
         self.__fig = plt.figure('residual')
         self.__ax = self.__fig.add_subplot(1, 1, 1)
 
+        # 現在のパラメータを中心にプロットするためのrangeを計算
+        self._reset_range()
+
+        # 現在のパラメータを履歴に追加
+        current_param = self.__model.get_param()
+        self._param_history[0].append(current_param[0])
+        self._param_history[1].append(current_param[1])
+
+        # パラメータを書き換えるのでコピーして参照している他のモジュールへの影響を防ぐ
+        model = deepcopy(self.__model)
+        Z = self._compute_residual_distribution(model)
+
+        im = self.__ax.pcolormesh(self.__X, self.__Y, Z, cmap='hsv')
+        self.__ax.plot(self._param_history[0], self._param_history[1], marker='.')
+        self.__fig.colorbar(im)
+
+        plt.pause(0.01)
+
+    def _reset_range(self):
+        param = self.__model.get_param()
+        if len(param) != 2:
+            raise Exception('Param2DPlotter support only 2D parameter')
+        param_range = self._param_range
+        grid_num = self._grid_num
+        x = np.arange(
+                param[0] - param_range[0], param[0] + param_range[0],
+                (2. * param_range[0]) / grid_num)
+        y = np.arange(
+                param[1] - param_range[1], param[1] + param_range[1],
+                (2. * param_range[1]) / grid_num)
+        if len(x) > len(y):
+            x = np.delete(x, -1)
+        elif len(y) > len(x):
+            y = np.delete(y, -1)
+        self.__X, self.__Y = np.meshgrid(x, y)
+
+    def _compute_residual_distribution(self, model):
         Z = []
         for i in range(len(self.__X)):
             Z_ele = []
             for j in range(len(self.__Y)):
-                self.__model.update([self.__X[i][j], self.__Y[i][j]])
+                # 書き換え
+                model.update([self.__X[i][j], self.__Y[i][j]])
                 ess = 0
                 for k in range(len(self.__data)):
                     # 誤差があまりにも大きくなることがあるので対数にする
                     # 大小関係は変わらない
-                    ess += np.log(self.__model.residual(self.__data[k])**2)
+                    ess += np.log(model.residual(self.__data[k])**2)
                 Z_ele.append(ess)
             Z.append(Z_ele)
         Z = np.array(Z)
 
-        im = self.__ax.pcolormesh(self.__X, self.__Y, Z, cmap='hsv')
-        self.__fig.colorbar(im)
-
-        plt.pause(0.01)
+        return Z
